8e206a3189a411a3767d8388c77d84e665b8171d
CaptinCornflakes <ztrevino@ymail.com>
2022-03-03 17:47:47 -0500

    Update main.java

--- src/main/java/mcsync/main.java
+++ src/main/java/mcsync/main.java
@@ -5,7 +5,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-
 import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
